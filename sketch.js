81e316d8c355053b02fa9c1ce5ac4ad3686cfae4
Tylerwhitehat <70729291+Tylerwhitehat@users.noreply.github.com>
2020-09-22 20:01:06 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+var pig1;
+var log1;
+var box4;
+var pig2;
+var log2;
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+   
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    ground1 = new Ground(600,height,1200,20)
+   // pig1= new pig(810,350)
+   // log1=new log(810,260,300,PI/2)
+   // box3=new Box(700,240,70,70);
+  //  box4= new Box(920,240,70,70);
+  //  pig2=new pig(810,220);
+  //  log2= new log(810,180,300,PI/2);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    
+    box1.display();
+    box2.display();
+    ground1.display();
+   /* pig1.display();
+    log1.display();
+    box3.display();
+    box4.display();
+    pig2.display();
+    log2.display();*/
+}
